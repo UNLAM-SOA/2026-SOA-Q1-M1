a43a469b13c95141b4a243121507464e8603ae56
Federico Martucci <federico@unsam.edu.ar>
2026-05-31 00:18:26 +0000

    feat(android): add Splash, Login and Dashboard activities
    Tres Activities encadenadas con Intents explicitos:
    - SplashActivity: Handler.postDelayed(2s) y lanza LoginActivity
    - LoginActivity: form con email/password, al submit pasa user al Dashboard via putExtra
    - DashboardActivity: lee getIntent().getExtras() y muestra el usuario logueado
    
    Manifest:
    - Declara las 3 Activities
    - Launcher = SplashActivity
    - Theme global Theme.PawGate

---
+++ android/app/src/main/java/com/unlam/pawgate/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.unlam.pawgate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText user;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        this.user = findViewById(R.id.login_email_input);
+        this.password = findViewById(R.id.login_password_input);
+        this.login = findViewById(R.id.login_submit_button);
+
+        this.login.setOnClickListener(v -> {
+            Intent i = new Intent(this, DashboardActivity.class);
+            i.putExtra("user", this.user.getText().toString());
+            i.putExtra("password", this.password.getText().toString());
+            startActivity(i);
+            finish();
+        });
+    }
+
+
+}
---
+++ android/app/src/main/java/com/unlam/pawgate/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.unlam.pawgate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        Intent i = new Intent(this, LoginActivity.class);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
