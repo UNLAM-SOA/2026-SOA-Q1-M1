09e738775e2d04bad031afa44b15baf6d0722628
Federico Martucci <federicomartucci@hotmail.com>
2026-06-07 23:53:07 +0000

    chore(android/dashboard): import View que quedo pendiente del commit anterior
    actionOpen y actionCall son View pero el import no se incluyo en el
    commit que los introdujo. Sin esto el build falla con 'cannot find
    symbol class View'.

--- Android/app/src/main/java/com/unlam/pawgate/DashboardActivity.java
+++ Android/app/src/main/java/com/unlam/pawgate/DashboardActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
